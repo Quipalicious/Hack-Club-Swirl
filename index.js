7c8d59b8ba799f6c54ba03f4abdb287c2547f0da
Quipalicious <saharsh6303@gmail.com>
2025-09-28 23:04:18 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var text = [
         '_',
         '_',
-        '_',
-        '_',
         'm_',
         'ma_',
         'ma_',
         'mak_',
-        'make_',
-        'make_',
-        'make',
-        'make',
         'make_',
         'make_',
         'make',
@@ -31,9 +25,6 @@ var text2 = [
     'art ',
     'art_',
     'art_',
-    'art ',
-    'art ',
-    'art_'
 ]
 
 var i = 0;
@@ -47,25 +38,29 @@ function typeWriter() {
     } else {
         i = 0;
         typeWriter2();
-        console.log("typewrites shourld bew gours");
         artAnimation();
     }
 }
 
 function typeWriter2(){
     if(i < text2.length){
-        console.log("runingmean>?")
         document.getElementById("art").innerHTML = text2[i];
         i++;
         setTimeout(typeWriter2, speed);
     }else{
         document.getElementById("art").classList.add("rainbow");
+        setTimeout(shrink, 2000);
     }
 }
 
 function artAnimation(){
     document.getElementById("art").style.display = 'block';
     document.getElementById("make").classList.add("scroll");
+}
+
+function shrink(){
+    document.getElementById("intro").classList.add("shrink");
+    document.getElementById("main").style.display = 'block';
 }
 
 typeWriter();
